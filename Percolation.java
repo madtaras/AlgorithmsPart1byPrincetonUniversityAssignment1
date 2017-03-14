95c2e8ef715ca58187e08fd78affaad8981bb78e
Taras Mazurkevych <mad.taras@gmail.com>
2017-03-14 20:22:30 +0200

    Initial commit

---
+++ Percolation.java
@@ -0,0 +1,119 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private int size;
+    private WeightedQuickUnionUF weightedQuickUnionUFMatrix;
+    private boolean[][] helpingMatrix;
+
+    public Percolation(int n) {
+        size = n;
+        weightedQuickUnionUFMatrix = new WeightedQuickUnionUF(n * n + 2);
+        // matrix size is n + 1 for indeces to start from 1
+        helpingMatrix = new boolean[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                helpingMatrix[i][j] = false;
+            }
+        }
+    }
+
+    public void open(int row, int col) {
+        // validate indices
+        if (!validateIndices(row, col)) {
+            return;
+        }
+
+        // don't perform actions if the site is opened, except first row sites
+        if (!helpingMatrix[row][col] || row == 1) {
+            // open site (row, col) if it is not open already
+            helpingMatrix[row][col] = true;
+
+            // connect just opened site with its open neighbors
+            // connect to top site
+            int topRow = row - 1;
+
+            if (validateIndices(topRow, col) &&
+                    helpingMatrix[topRow][col]) {
+                weightedQuickUnionUFMatrix.union(xyTo1D(row, col), xyTo1D(topRow, col));
+            }
+
+            // connect to middle sites
+            if (validateIndices(row, col - 1) &&
+                    helpingMatrix[row][col - 1]) {
+                weightedQuickUnionUFMatrix.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+
+            }
+            if (validateIndices(row, col + 1) &&
+                    helpingMatrix[row][col + 1]) {
+                weightedQuickUnionUFMatrix.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+
+            }
+
+            // connect to bottom site
+            int bottomRow = row + 1;
+            if (validateIndices(bottomRow, col) &&
+                    helpingMatrix[bottomRow][col]) {
+                weightedQuickUnionUFMatrix.union(xyTo1D(row, col), xyTo1D(bottomRow, col));
+
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return helpingMatrix[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row == 1) {
+            return helpingMatrix[row][col];
+        }
+
+        for (int i = 1; i < size + 1; i++) {
+            if (weightedQuickUnionUFMatrix.connected(xyTo1D(1, i),
+                    xyTo1D(row, col))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        int numberOfOpenSitesInt = 0;
+        for (int i = 1; i < size + 1; i++) {
+            for (int j = 1; j < size + 1; j++) {
+                if (helpingMatrix[i][j]) numberOfOpenSitesInt++;
+            }
+        }
+
+        return numberOfOpenSitesInt;
+    }
+
+    public boolean percolates() {
+        if (size == 1 && !helpingMatrix[1][1]) {
+            return false;
+        }
+
+        for (int i = 1; i < size + 1; i++) {
+            for (int j = 1; j < size + 1; j++) {
+                if (weightedQuickUnionUFMatrix.connected(xyTo1D(1, i),
+                        xyTo1D(size, j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int xyTo1D(int row, int col) {
+        return size * (row - 1) + col - 1;
+    }
+
+    private boolean validateIndices(int row, int col) {
+        if (row < 1 || row > size || col < 1 || col > size) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) { }
+}
---
+++ PercolationStats.java
@@ -0,0 +1,56 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private int num;
+    private int trials;
+    private double[] numsOfSteps;
+
+    public PercolationStats(int n, int t) {
+        if (n <= 0 || t <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        num = n;
+        trials = t;
+
+        int numOfSteps;
+        numsOfSteps = new double[num];
+        Percolation perc;
+        for (int i = 0; i < trials; i++) {
+            perc = new Percolation(num);
+            numOfSteps = 0;
+            while (!perc.percolates()) {
+                perc.open(StdRandom.uniform(1, num + 1), StdRandom.uniform(1, num + 1));
+                numOfSteps += 1;
+            }
+            numsOfSteps[i] = numOfSteps * 1.0 / (num * num);
+        }
+        System.out.println("mean                    = " + Double.toString(this.mean()));
+        System.out.println("stddev                  = " + Double.toString(this.stddev()));
+        System.out.println("95% confidence interval = [" + Double.toString(this.confidenceLo()) +
+                ", " + Double.toString(this.confidenceHi()) + "]");
+    }
+
+    public double mean() {
+        return StdStats.mean(this.numsOfSteps);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(this.numsOfSteps);
+    }
+
+    public double confidenceLo() {
+        return this.mean() - (1.96 * this.stddev() / Math.sqrt(this.trials));
+    }
+
+    public double confidenceHi() {
+        return this.mean() + (1.96 * this.stddev() / Math.sqrt(this.trials));
+    }
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats percStats = new PercolationStats(num, trials);
+    }
+}
